2bc101f3045ce52d0254c6b9b696ecf053063f5c
Lucas Polesello <lucas.c4d@gmail.com>
2022-01-05 19:51:59 -0300

    networking and route working

---
+++ src/components/PollView/PollView.js
@@ -0,0 +1,41 @@
+import { useParams } from 'react-router-dom'
+import { fetchPollById } from '../../store/slices/Poll'
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+export default function PollView() {
+
+    const { pollId } = useParams()
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchPollById(pollId));
+    }, [pollId])
+
+
+    const {
+        id,
+        subject,
+        description,
+        expiresAt,
+        reportedAt,
+        usersVotes,
+        favor,
+        against,
+        result,
+        creator,
+    } = useSelector((state) => state.poll.poll_item);
+
+    return (
+        <>
+            <p>{id}</p>
+            <p>{subject}</p>
+            <p>{description}</p>
+            <p>{expiresAt}</p>
+            <p>{reportedAt}</p>
+            <p>{result}</p>
+            <p>{favor}</p>
+            <p>{against}</p>
+        </>
+    )
+}
